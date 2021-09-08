744b346ebfcf963ad20f9ae0721f96c9470f6635
Prabodh Kakodkar <prabodhkakodkar@gmail.com>
2021-09-08 16:54:21 +0100

    add tests for [POST] /subscriptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Result};
 use std::io::Error;
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, Error> {
+pub fn run(listener: TcpListener) -> Result<Server, Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
 
     Ok(server)
