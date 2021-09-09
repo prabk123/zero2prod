db73267d2f5017490536bfbf84a4a5d5c6516271
Prabodh Kakodkar <prabodhkakodkar@gmail.com>
2021-09-09 01:09:41 +0100

    add sqlx and db assertion

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port.");
+    println!("TESTING");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let startup_message = format!("> Starting server on http://{}", address);
+    let listener = TcpListener::bind(address).expect("Failed to bind random port.");
+
+    println!("{}", startup_message);
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 
 #[actix_rt::test]
 async fn health_check_works() {
@@ -34,6 +36,11 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Arrange
     let address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
@@ -48,6 +55,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[actix_rt::test]
@@ -57,35 +72,35 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     - checks if the endpoint is behind a POST request
     - checks if endpoint returns a 400 status code if data is missing
     */
-    
-		// Arrange
+
+    // Arrange
     let address = spawn_app();
     let client = reqwest::Client::new();
-		let test_cases = vec![
-			("name=le%20guin", "missing the email"),
-			("email=ursula_le_guin%40gmail.com", "missing the name"),
-			("", "missing both name and email")
-		];
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
 
-		for (invalid_body, error_message) in test_cases {
-			 // Act
-			 let response = client
-			 .post(&format!("{}/subscriptions", address))
-			 .header("Content-Type", "application/x-www-form-urlencoded")
-			 .body(invalid_body)
-			 .send()
-			 .await
-			 .expect("Failed to execute request.");
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
 
-			 // Assert
-			 assert_eq!(
-				 400,
-				 response.status().as_u16(),
-				 // Additional customised error message on test failure
-				 "The API did not fail with 400 Bad Request when the payload was {}.",
-				 error_message
-			 );
-		}
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
 
 fn spawn_app() -> String {
