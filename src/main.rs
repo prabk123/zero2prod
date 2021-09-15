4d9bfccb9351ddc5824c6962e4c078f707f747d6
Prabodh Kakodkar <prabodhkakodkar@gmail.com>
2021-09-15 19:41:23 +0100

    add telemetry as a mod

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -14,7 +14,8 @@ async fn main() -> std::io::Result<()> {
     let startup_message = format!("> Starting server on http://{}", address);
     let listener = TcpListener::bind(address).expect("Failed to bind random port.");
 
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     println!("{}", startup_message);
     run(listener, connection_pool)?.await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,7 +9,15 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    log::info!("Saving new subscriber details in the database");
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name= %form.name
+    );
+    let _request_span_guard = request_span.enter();
+    let _query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,12 +31,9 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => {
-            log::info!("New subscriber details have been saved");
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
