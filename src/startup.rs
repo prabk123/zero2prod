1e377e7ef741912b58999cf62bca0f8b5761fbe1
Prabodh Kakodkar <prabodhkakodkar@gmail.com>
2021-09-15 20:42:59 +0100

    add TracingLogger

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,15 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::middleware::Logger;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::io::Error;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, Error> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
