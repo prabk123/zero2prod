a24988fddbe6270759466ba2036677db9937c0d2
prabk123 <prabodhkakodkar@gmail.com>
2021-10-30 15:45:16 +0100

    add db connection timeout of 2s

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -7,10 +7,15 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
-    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let startup_message = format!("> Starting server on http://{}", address);
     let listener = TcpListener::bind(address).expect("Failed to bind random port.");
 
